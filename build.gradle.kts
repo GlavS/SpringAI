8ddfa62deabf5e10f36138b6c5d529696169628e
GlavS <pianoforte2@yandex.ru>
2025-12-01 14:01:46 +0300

    Added logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
-    id("java")
+    java
+    application
+    id("com.diffplug.spotless") version "6.25.0"
+}
+
+application {
+    mainClass = "ru.otus.Hw1"
 }
 
 group = "ru.otus"
@@ -10,9 +16,33 @@ repositories {
 }
 
 dependencies {
+    implementation("org.slf4j:slf4j-api:2.0.13")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+spotless {
+    java {
+        palantirJavaFormat("2.74.0")
+
+        // Эти две опции многие команды используют всегда
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+
+    kotlin {
+        ktlint()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+
+    kotlinGradle {
+        ktlint()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
 
 tasks.test {
---
+++ src/main/java/ru/otus/Hw1.java
@@ -0,0 +1,12 @@
+package ru.otus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Hw1 {
+    private static final Logger log = LoggerFactory.getLogger(Hw1.class);
+
+    static void main() {
+        log.info("Initial setup - done");
+    }
+}
