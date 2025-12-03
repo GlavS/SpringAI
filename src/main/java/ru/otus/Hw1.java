f5416884b83cc8fb3b56d304d123ec36bdf72d19
GlavS <pianoforte2@yandex.ru>
2025-12-03 21:40:02 +0300

    ConsoleIOService update

--- src/main/java/ru/otus/Hw1.java
+++ src/main/java/ru/otus/Hw1.java
@@ -2,11 +2,15 @@ package ru.otus;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.otus.console.ConsoleIOService;
+import ru.otus.console.IOService;
 
 public class Hw1 {
     private static final Logger log = LoggerFactory.getLogger(Hw1.class);
 
     static void main() {
         log.info("HW1 started");
+        IOService service = new ConsoleIOService(System.in, System.out);
+        service.printLineF("Your choice is -> %d%n", service.readChoice());
     }
 }
