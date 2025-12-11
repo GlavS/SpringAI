d0e41e7a5cfecc881556f5e358af3bb6effdcd59
GlavS <pianoforte2@yandex.ru>
2025-12-12 01:55:23 +0300

    Division complete

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,76 +1,66 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-
 plugins {
-    java
-    application
-    id("com.diffplug.spotless") version "6.25.0"
-    id("com.gradleup.shadow") version "8.3.9"
-    id ("name.remal.sonarlint") version "7.0.0"
-}
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(25))
-    }
+    id("com.diffplug.spotless") version "6.25.0" apply false
+    id("com.gradleup.shadow") version "8.3.9" apply false
+    id("name.remal.sonarlint") version "7.0.0" apply false
 }
 
-application {
-    mainClass = "ru.otus.Hw1"
-}
+
+
 
 group = "ru.otus"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    // https://mvnrepository.com/artifact/org.springframework/spring-context
-    implementation("org.springframework:spring-context:7.0.1")
-    implementation("org.slf4j:slf4j-api:2.0.13")
-    implementation("ch.qos.logback:logback-classic:1.5.21")
-    // https://mvnrepository.com/artifact/com.opencsv/opencsv
-    implementation("com.opencsv:opencsv:5.12.0")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    // https://mvnrepository.com/artifact/org.mockito/mockito-core
-    testImplementation("org.mockito:mockito-junit-jupiter:5.20.0")
-    testImplementation("org.assertj:assertj-core:3.27.6")
-
-}
-
-spotless {
-    java {
-        palantirJavaFormat("2.74.0")
-
-        // Эти две опции многие команды используют всегда
-        trimTrailingWhitespace()
-        endWithNewline()
+allprojects {
+    repositories {
+        mavenCentral()
     }
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "com.diffplug.spotless")
+    apply(plugin = "com.gradleup.shadow")
+    apply(plugin = "name.remal.sonarlint")
 
-tasks.withType<ShadowJar> {
-    // На всякий случай, чтобы файлы из META-INF не выкидывались
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    extensions.configure<JavaPluginExtension> {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+    }
 
-    // Общее слияние service-файлов (META-INF/services/**)
-    mergeServiceFiles()
+    dependencies {
+        add("testImplementation", platform("org.junit:junit-bom:5.10.0"))
+        add("testImplementation", "org.junit.jupiter:junit-jupiter")
+        add("testRuntimeOnly", "org.junit.platform:junit-platform-launcher")
 
-    // Конкретно для Spring — склеить текст этих файлов
-    append("META-INF/spring.handlers")
-    append("META-INF/spring.schemas")
-}
+        add("testImplementation", "org.mockito:mockito-junit-jupiter:5.20.0")
+        add("testImplementation", "org.assertj:assertj-core:3.27.6")
+    }
 
-tasks.withType<JavaExec> {
-    standardInput = System.`in`
-}
 
-sonarLint {
-    ignoredPaths.add("src/test/java/Runner.java")
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    extensions.configure<SpotlessExtension> {
+        java {
+            palantirJavaFormat("2.74.0")
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+    }
+
+
+    tasks.withType<JavaExec> {
+        standardInput = System.`in`
+    }
+
+    tasks.withType<ShadowJar> {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        mergeServiceFiles()
+        append("META-INF/spring.handlers")
+        append("META-INF/spring.schemas")
+    }
 }
--- hw01-spring-core-xml/build.gradle.kts
+++ hw01-spring-core-xml/build.gradle.kts
@@ -1,74 +1,16 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
-
 plugins {
-    java
     application
-    id("com.diffplug.spotless") version "6.25.0"
-    id("com.gradleup.shadow") version "8.3.9"
-    id ("name.remal.sonarlint") version "7.0.0"
-}
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(25))
-    }
 }
 
 application {
     mainClass = "ru.otus.Hw1"
 }
 
-group = "ru.otus"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    // https://mvnrepository.com/artifact/org.springframework/spring-context
     implementation("org.springframework:spring-context:7.0.1")
     implementation("org.slf4j:slf4j-api:2.0.13")
     implementation("ch.qos.logback:logback-classic:1.5.21")
-    // https://mvnrepository.com/artifact/com.opencsv/opencsv
     implementation("com.opencsv:opencsv:5.12.0")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    // https://mvnrepository.com/artifact/org.mockito/mockito-core
-    testImplementation("org.mockito:mockito-junit-jupiter:5.20.0")
-    testImplementation("org.assertj:assertj-core:3.27.6")
-
-}
-
-spotless {
-    java {
-        palantirJavaFormat("2.74.0")
-
-        // Эти две опции многие команды используют всегда
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<ShadowJar> {
-    // На всякий случай, чтобы файлы из META-INF не выкидывались
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
-
-    // Общее слияние service-файлов (META-INF/services/**)
-    mergeServiceFiles()
-
-    // Конкретно для Spring — склеить текст этих файлов
-    append("META-INF/spring.handlers")
-    append("META-INF/spring.schemas")
-}
-
-tasks.withType<JavaExec> {
-    standardInput = System.`in`
 }
 
 sonarLint {
