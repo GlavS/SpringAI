f9fa676cf41bd82aa2c6ead3b9909de37b39fd94
GlavS <pianoforte2@yandex.ru>
2025-12-12 21:15:51 +0300

    Docker compose for Postgres added

--- hw02-jdbc-spring-annotations/build.gradle.kts
+++ hw02-jdbc-spring-annotations/build.gradle.kts
@@ -6,3 +6,7 @@ application {
     mainClass = "ru.otus.Hw2"
 }
 
+dependencies {
+    implementation("org.springframework:spring-context:7.0.1")
+    implementation("ch.qos.logback:logback-classic:1.5.21")
+}
--- hw02-jdbc-spring-annotations/src/main/java/ru/otus/Hw2.java
+++ hw02-jdbc-spring-annotations/src/main/java/ru/otus/Hw2.java
@@ -1,8 +1,13 @@
 package ru.otus;
 
-@SuppressWarnings("java:S106")
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Hw2 {
+
+    private static final Logger log = LoggerFactory.getLogger(Hw2.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        log.info("Hello, {}", "мир!");
     }
 }
