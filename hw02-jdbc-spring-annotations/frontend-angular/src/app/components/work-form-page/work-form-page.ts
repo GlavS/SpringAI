beb2ff66dd4ccc9db786bba0643a55006a169990
GlavS <pianoforte2@yandex.ru>
2026-01-13 00:11:27 +0300

    Dumb create form added

--- hw02-jdbc-spring-annotations/frontend-angular/src/app/components/work-form-page/work-form-page.ts
+++ hw02-jdbc-spring-annotations/frontend-angular/src/app/components/work-form-page/work-form-page.ts
@@ -1,13 +1,32 @@
 import {Component, inject} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-work-form-page',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './work-form-page.html',
   styleUrl: './work-form-page.css',
 })
+
 export class WorkFormPage {
   private route = inject(ActivatedRoute);
   protected mode:string = this.route.snapshot.paramMap.has("id")? 'EDIT' : 'CREATE';
+
+  workForm = new FormGroup(
+    {
+      title: new FormControl('')
+    }
+  );
+
+  submitValue(){
+    if(this.mode === 'CREATE'){
+      console.log('Creating title: ', this.workForm.value.title);
+    } else {
+      console.log('Editing title: ', this.workForm.value.title);
+    }
+  }
 }
